261970e3b81b5a64ef96c7d0fe6a6c09ac2f4f55
Sebastian García <sebastiangarcia@MacBook-Pro-de-Sebastian.local>
2025-03-27 10:26:18 -0500

    update 2

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 import plotly.express as px
 
 def load_csv_data(vehicles_us):
-    vehicles_us = pd.read_csv('/Users/sebastiangarcia/GitHub/project-/vehicles_us.csv')
+    vehicles_us = pd.read_csv('vehicles_us.csv')
     
-if __name__ == "__main__":
-    load_csv_data('/Users/sebastiangarcia/GitHub/project-/vehicles_us.csv')
+load_csv_data('vehicles_us.csv')
 
-st.header("Car Dealership Explorer")
+
+st.title("Car Dealership Explorer")
 
 st.markdown("""
-This App perfoms a simple comparation in between models and prices
+This App perfoms a simple comparation in between car models and prices
 """)

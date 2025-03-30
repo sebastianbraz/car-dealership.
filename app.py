e0495578267758b24ce935118b18da4e8d4db700
Sebastian García <sebastiangarcia@MacBook-Pro-de-Sebastian.local>
2025-03-30 12:36:56 -0500

    update 20

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ vehicles_us['model_year'].fillna(vehicles_us['model_year'].median(), inplace=Tru
 
 #price
 
-vehicles_us['price'] = pd.to_numeric(vehicles_us['price'], errors='coerce').astype('str')
+#vehicles_us['price'] = pd.to_numeric(vehicles_us['price'], errors='coerce').astype('str')
 vehicles_us['price'].fillna(0.0, inplace=True)  
 
-vehicles_us['days_listed'] = pd.to_numeric(vehicles_us['days_listed'], errors='coerce').astype('str')
+#vehicles_us['days_listed'] = pd.to_numeric(vehicles_us['days_listed'], errors='coerce').astype('str')
 vehicles_us['days_listed'].fillna(0.0, inplace=True)  
 
 #Cylinders
